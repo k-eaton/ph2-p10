8beee9a2a57a69e5532e00d3f032bb6d79e1f06d
Katrina Eaton <katrina.eaton@gmail.com>
2015-03-29 13:35:52 -0700

    reconsile branch differences

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,11 +1,18 @@
+require 'httparty'
+
 get '/' do
   # Look in app/views/index.erb
   erb :index
 end
 
-post '/add_todo' do
-  @new_todo = Todo.create(todo_content: params[:todo])
 
-  redirect '/'
+
+post '/new_search' do
+  p @new_search = params[:search_term].split(' ').join("+")
+  content_type :json
+  @new_search.to_json
+  response = HTTParty.get('https://archive.org/advancedsearch.php?q=title%3A'+@new_search +'+collection%3A+prelinger+downloads%3A%5B1000+TO+100000000%5D&fl%5B%5D=avg_rating&fl%5B%5D=description&fl%5B%5D=identifier&fl%5B%5D=num_reviews&fl%5B%5D=title&fl%5B%5D=year&sort%5B%5D=&sort%5B%5D=&sort%5B%5D=&rows=50&page=1&output=json')
+  p response['response']['docs'][0]['description']
+  # redirect "/"
 end
 
